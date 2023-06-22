63600f400609e9506e3e8009fc546baf1ec08a56
Daniel Burgess <dan-burgess@hotmail.com>
2023-06-22 23:54:03 +0930

    add html for signup box

--- newsletter-sign-up/src/App.js
+++ newsletter-sign-up/src/App.js
@@ -3,23 +3,23 @@ const Signup = () => {
   return (
     <div>
       <div className="container">
-    <div className="info">
-      <h2>Stay updated!</h2>
-      <p>Join 60,000+ product managers receiving monthly updates on:</p>
-      <ul>
-        <li>Product discovery and building what matters</li>
-        <li>Measuring to ensure updates are a success</li>
-        <li>And much more!</li>
-      </ul>
-      <p>Email address</p>
-      <form>
-        <input type="text" placeholder="email@company.com" />
-        <button type="submit">Subscribe to monthly newsletter</button>
-      </form>
-    </div>
-    <div>
-    </div>
-  </div>
+        <div className="info">
+          <h2>Stay updated!</h2>
+          <p>Join 60,000+ product managers receiving monthly updates on:</p>
+          <ul>
+            <li>Product discovery and building what matters</li>
+            <li>Measuring to ensure updates are a success</li>
+            <li>And much more!</li>
+          </ul>
+          <p>Email address</p>
+          <form>
+            <input type="text" placeholder="email@company.com" />
+            <button type="submit">Subscribe to monthly newsletter</button>
+          </form>
+        </div>
+        <div>
+        </div>
+      </div>
     </div>
   )
 }
